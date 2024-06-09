84838ff0c7f68ee8225bd149df75a1c1288ca62d
Marabiks <marfikasdev@gmail.com>
2024-06-09 10:18:21 -0600

    Refactor home page.

--- src/app/challenge.service.ts
+++ src/app/challenge.service.ts
@@ -6,4 +6,69 @@ import { Injectable } from '@angular/core';
 export class ChallengeService {
 
   constructor() { }
+
+  getResult(num: number): Result {
+    return {
+      list: this.getList(num).join(', '),
+      multiples3: this.getMultiples(3, num).join(', '),
+      multiples5: this.getMultiples(5, num).join(', '),
+      multiples7: this.getMultiples(7, num).join(', '),
+      listWithColors: this.getListWithColors(num),
+    }
+  }
+
+  private getList(limit: number) {
+    const list: number[] = []
+    for (let i = 0; i <= limit; i++) {
+      list.push(i)
+    }
+    return list
+  }
+
+  private getMultiples(num: number, limit: number) {
+    const list: number[] = []
+    for (let i = 0; i <= limit; i += num) {
+      list.push(i)
+    }
+    return list
+  }
+
+  private getListWithColors(limit: number) {
+    const list: ListWithColorsItem[] = []
+    for (let i = 0; i <= limit; i++) {
+      const item: ListWithColorsItem = { num: i, colorPrimary: '#000000', colorSecondary: '#e6e6e6' } // Initialize with default color.
+      // Revisar las condiciones del menor valor al mayor tiene el efecto de seleccionar el color del menor múltiplo.
+      if (i % 3 === 0) {
+        item.colorPrimary = '#666666'
+        item.colorSecondary = '#e2f8e8'
+      } else if (i % 5 === 0) {
+        item.colorPrimary = '#a91e1e'
+        item.colorSecondary = '#ffdcdc'
+      } else if (i % 7 === 0) {
+        item.colorPrimary = '#0f58bd'
+        item.colorSecondary = '#ddedfd'
+      }
+      list.push(item)
+    }
+    return list
+  }
+}
+
+export interface Request {
+  input: number;
+  result: Result;
+}
+
+export interface Result {
+  list: string;
+  multiples3: string;
+  multiples5: string;
+  multiples7: string;
+  listWithColors: ListWithColorsItem[];
+}
+
+export interface ListWithColorsItem {
+  num: number;
+  colorPrimary: string;
+  colorSecondary: string;
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import { FormsModule, NgControl } from '@angular/forms';
 
 import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonTitle, IonToast, IonToolbar } from '@ionic/angular/standalone';
 
+import { ChallengeService, Result } from '../challenge.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -14,7 +16,12 @@ import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList,
 export class HomePage {
 
   inpNum = ''
-  result: any | undefined
+  result: Result | undefined
+
+  constructor(
+    private challengeService: ChallengeService,
+  ) {
+  }
 
   @ViewChild('inpNumControlTemplate')
   inpNumControl!: NgControl
@@ -29,71 +36,11 @@ export class HomePage {
     }
 
     const num = Number(this.inpNum)
-    this.result = this.getResult(num)
+    this.result = this.challengeService.getResult(num)
   }
 
   reset() {
     this.result = undefined
     this.inpNumControl.control?.reset('')
   }
-
-  private getResult(num: number): Result {
-    return {
-      list: this.getList(num).join(', '),
-      multiples3: this.getMultiples(3, num).join(', '),
-      multiples5: this.getMultiples(5, num).join(', '),
-      multiples7: this.getMultiples(7, num).join(', '),
-      listWithColors: this.getListWithColors(num),
-    };
-  }
-
-  private getList(limit: number) {
-    const list: number[] = []
-    for (let i = 0; i <= limit; i++) {
-      list.push(i)
-    }
-    return list
-  }
-
-  private getMultiples(num: number, limit: number) {
-    const list: number[] = []
-    for (let i = 0; i <= limit; i += num) {
-      list.push(i)
-    }
-    return list
-  }
-
-  private getListWithColors(limit: number) {
-    const list: ListWithColorsItem[] = []
-    for (let i = 0; i <= limit; i++) {
-      const item: ListWithColorsItem = { num: i, colorPrimary: '#000000', colorSecondary: '#e6e6e6' } // Initialize with default color.
-      // Revisar las condiciones del menor valor al mayor tiene el efecto de seleccionar el color del menor múltiplo.
-      if (i % 3 === 0) {
-        item.colorPrimary = '#666666'
-        item.colorSecondary = '#e2f8e8'
-      } else if (i % 5 === 0) {
-        item.colorPrimary = '#a91e1e'
-        item.colorSecondary = '#ffdcdc'
-      } else if (i % 7 === 0) {
-        item.colorPrimary = '#0f58bd'
-        item.colorSecondary = '#ddedfd'
-      }
-      list.push(item)
-    }
-    return list
-  }
-}
-
-interface Result {
-  list: string;
-  multiples3: string;
-  multiples5: string;
-  multiples7: string;
-  listWithColors: ListWithColorsItem[];
-}
-
-interface ListWithColorsItem {
-  num: number;
-  colorPrimary: string;
-  colorSecondary: string;
 }
