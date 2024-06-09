91e32615ed19402703698f5b8338e0ff4f6c2040
Marabiks <marfikasdev@gmail.com>
2024-06-08 19:56:29 -0600

    Set up ionic router outlet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IonApp, IonRouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
